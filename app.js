ce32c38ab465abc85b413420cad65ad8ba94583c
Manish Garg <manishgargpro@gmail.com>
2017-07-29 01:45:53 -0400

    ehh, coming along i guess

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 console.log("linked");
 
-$("button").click(function() {
+$(".search-button").click(function() {
 	var gifsSearch = $("input").val();
 	console.log(gifsSearch);
 	var gifsURL = encodeURIComponent(gifsSearch);
 	console.log(gifsURL);
+	$(".gif-container").empty();
 	getGifs(gifsURL);
+	createButtons();
 });
 
-$(".downsized-still").click(function() {
-	console.log("clicking works");
+$(".key-button").click(function(){
+	console.log("clicking key works")
 })
 
 var page = 0;
+
+var buttonArray = [];
 
 function getGifs(searchTerm) {
 	var settings = {
@@ -29,17 +33,23 @@ function getGifs(searchTerm) {
 			$(".gif-container").append(staticGif);
 		}
 		$(".page").html(page);
+		$(".search-result").click(function() {
+			if ($(this).hasClass("downsized-still")) {
+				$(this).replaceWith(createGifs(this, response));
+			}if ($(this).hasClass("original-moving")){
+				console.log("works")
+				$(this).replaceWith(createStills(this, response));
+			}
+		});
 	});
 }
-
-//function paginate()
 
 function createResults(data) {
 	var container = document.createElement('div');
 	var gifStatic = document.createElement('img');
 	var gifRating = document.createElement('span');
 	$(gifStatic).attr({
-			"class": "img-responsive downsized-still",
+			"class": "img-responsive search-result downsized-still",
 			"src": data.images.downsized_still.url,
 		}),
 		gifRating.innerHTML = "Rating: " + data.rating;
@@ -49,3 +59,38 @@ function createResults(data) {
 	$(container).addClass("panel-body")
 	return container;
 }
+
+function createButtons(){
+	//if (){
+		var button = document.createElement('button');
+		$(button).html($("input").val());
+		$(button).addClass("btn btn-default key-button");
+		$(".button-container").append(button);
+	//}
+}
+
+function createGifs(gif, response){
+	console.log("clicking result works");
+	var index = $(gif).index();
+	console.log(index);
+	var gifMoving = document.createElement('img');
+	$(gifMoving).attr({
+			"class": "img-responsive search-result original-moving",
+			"src": response.data[index].images.original.url,
+		});
+	return gifMoving;
+}
+
+function createStills(gif, response){
+	console.log("clicking result works");
+	var index = $(gif).index();
+	console.log(index);
+	var gifStatic = document.createElement('img');
+	$(gifStatic).attr({
+			"class": "img-responsive search-result downsized-still",
+			"src": response.data[index].images.downsized_still.url,
+		});
+	return gifStatic;
+}
+
+//function paginate()
